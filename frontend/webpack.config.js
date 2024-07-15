a27175955629fbe38eaa0045fc906567f0de5b7e
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-07-15 16:59:18 -0400

    frontend setup and create first component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,3 +30,38 @@ module.exports = {
     }),
   ],
 };
+
+// const path = require("path");
+// const webpack = require("webpack");
+
+// module.exports = (env, argv) => {
+//   const isProduction = argv.mode === 'production';
+
+//   return {
+//     entry: "./src/index.js",
+//     output: {
+//       path: path.resolve(__dirname, "./static/frontend"),
+//       filename: "[name].js",
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: "babel-loader",
+//           },
+//         },
+//       ],
+//     },
+//     optimization: {
+//       minimize: isProduction,
+//     },
+//     plugins: [
+//       new webpack.DefinePlugin({
+//         "process.env.NODE_ENV": JSON.stringify(argv.mode), // Use mode from argv
+//       }),
+//     ],
+//     devtool: isProduction ? false : 'source-map', // Source maps in development mode
+//   };
+// };
